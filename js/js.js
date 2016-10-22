e1df44e01038d5cf5abcc82c51a3067a5921f8b0
zhou <2581691163@qq.com>
2016-10-22 14:06:47 +0800

    page

--- js/js.js
+++ js/js.js
@@ -1,3 +1,27 @@
-window.ondeviceorientation = function(e) {
-    console.log('alpha: ' + e.alpha + ' beta: ' + e.bata + ' gamma: ' + e.gama);
-}
+$(function() {
+    $('.nav a').click(function(e) {
+        var $this = $(this),
+            section = $(this).data('nav'),
+            nav = $('.navbar-nav');
+
+        if ($('[data-section=' + section + ']').length) {
+            $('html,body').animate({
+                scrollTop: $('[data-section=' + section + ']').offset().top
+            }, 500)
+        }
+        nav.find('.active').removeClass('active');
+        $this.parent().addClass('active');
+        e.preventDefault();
+
+    });
+    $(window).scroll(function(e) {
+        var header = $('.top-nav'),
+            scrolltop = $(this).scrollTop();
+        if (scrolltop > 500 && scrolltop < 2000) {
+            header.addClass('top-nav-fixed');
+        } else if (scrolltop <= 500) {
+            header.removeClass('top-nav-fixed');
+        }
+
+    })
+})
